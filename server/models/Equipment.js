2f2100a7e68b0c307eba910f85f89652dcfa1642
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-26 09:51:45 -0400

    built equipment model

---
+++ server/models/Equipment.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const equipSchema = new Schema({
+    equipName: {
+        type: String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    techLvl:{
+        type: Number,
+        required: true
+    },
+    range:{
+        type:Number,
+    },
+    magazineCap:{
+        type: Number
+    },
+    dmgDice:{
+        type: String
+    },
+    dmgType:{
+        type: String
+    },
+    reloadTime:{
+        type:String
+    },
+    bulk:{
+        type:Number,
+        required: true
+    },
+    category:{
+        type:String,
+        required:true
+    }
+
+
+})
+
+const Equipment = model('Equipment', equipSchema);
+module.exports= Equipment
